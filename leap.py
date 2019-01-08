b91e7a91a0019891d44786f0cfa634fb6e381085
joetituskumar <46440864+joetituskumar@users.noreply.github.com>
2019-01-08 16:00:43 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,8 +1,5 @@
 n=int(input())
-if n % 400 == 0:
+if n % 4==0 and n % 100 !=0 or n % 400==0:
   print("yes")
-elif n % 100==0:
-  print("no")
 else:
-  if n % 4== 0:
-    print("yes")
+  print("no")
