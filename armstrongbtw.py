f269e69e252e2b11e515b6c299a0eae0bc5fca7b
joetituskumar <46440864+joetituskumar@users.noreply.github.com>
2019-01-15 12:41:38 +0530

    Update armstrongbtw.py

--- armstrongbtw.py
+++ armstrongbtw.py
@@ -2,9 +2,14 @@ n,k=map(int,input().split())
 for i in range(n,k+1):
    temp=i
    sum1 = 0
-  while temp > 0:
-    digit =temp % 10
-    sum1=sum1+ digit**3
-    temp=temp // 10
-    if i== sum1:
-      print(i)
+   while temp > 0:
+      digit =temp % 10
+      sum1=sum1+ digit**3
+      temp=temp // 10
+      if i== sum1:
+         print(i)
+         
+        
+        
+   
+      
