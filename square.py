6a2452fb068ee4e7fc544a078c6da46ccf16f5c4
joetituskumar <46440864+joetituskumar@users.noreply.github.com>
2019-02-15 16:20:53 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,12 @@
-full=[int(x) for x in input().split()]
+x1,y1  = map(int,input().split())
+x2,y2  = map(int,input().split())
+x3,y3  = map(int,input().split())
+x4,y4  = map(int,input().split())
 emi=[]
-for i in full:
+for i in (x1,y1,x2,y2,x3,y3,x4,y4):
   if i not in emi:
     emi.append(i)
 if len(emi)==2:
   print("yes")
+else:
+  print("no")
